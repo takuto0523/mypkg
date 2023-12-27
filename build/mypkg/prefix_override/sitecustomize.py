a57026c222b6a7d188263395aeb42f2a023a3ec7
takuto0523 <takuto.bb.15.05.23@gmail.com>
2023-12-27 12:44:32 +0900

    Add a command

---
+++ build/mypkg/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/takuto0523/ros2_ws/src/mypkg/install/mypkg'
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'mypkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.launch.py'))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='takuto0523',
+    maintainer_email='takuto.bb.15.05.23@gmail.com',
+    description='a package for practice',
+    license='BSD-3-Clause',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'talker = mypkg.talker:main',
+            'listener = mypkg.listener:main',
+        ],
+    },
+)
